5b337ab7446d608696e880700d8347a2ab593c91
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-06-24 17:31:08 +0300

    @XYB-6: Added BetController

---
+++ XyBet/XYB.API/XYB.API/Controllers/BetController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using XYB.API.DTOs;
+
+namespace XYB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BetController : ControllerBase
+    {
+        public BetController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BetViewModel>>> GetBets()
+        {
+            return Ok(new List<BetViewModel> { new BetViewModel() });
+        }
+    }
+}
---
+++ XyBet/XYB.API/XYB.API/DTOs/BetViewModel.cs
@@ -0,0 +1,18 @@
+﻿using XYB.Data.Entities;
+using XYB.Data.Enums;
+
+namespace XYB.API.DTOs
+{
+    public class BetViewModel
+    {
+        public int Id { get; set; }
+        
+        public AppUser User { get; set; }
+
+        public double BetSum { get; set; }
+
+        public BetResult Result { get; set; } = BetResult.InProgress;
+
+        public GameMatch Match { get; set; }
+    }
+}
