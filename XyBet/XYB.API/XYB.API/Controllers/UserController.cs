0499f0d7b96984b282e8f59d12df72913d937608
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-06-25 23:21:33 +0300

    @XYB-8: Added user DTOs and mappings

--- XyBet/XYB.API/XYB.API/Controllers/UserController.cs
+++ XyBet/XYB.API/XYB.API/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using XYB.API.DTOs.User;
 using XYB.API.Services.Abstractions;
+using XYB.Data.Abstractions;
+using XYB.Data.Entities;
 
 namespace XYB.API.Controllers
 {
@@ -11,16 +15,27 @@ namespace XYB.API.Controllers
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
         private readonly ISignInManager _signInManager;
+        private readonly IUnitOfWork _uow;
 
         public UserController(
             ITokenService tokenService,
             IMapper mapper,
-            ISignInManager signInManager
+            ISignInManager signInManager,
+            IUnitOfWork uow
             )
         {
             _tokenService = tokenService;
             _mapper = mapper;
             _signInManager = signInManager;
+            _uow = uow;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserViewModel>> CreateUser([FromBody] UserCreateModel createModel)
+        {
+            var user = _mapper.Map<AppUser>(createModel);
+
+            _signInManager.
         }
     }
 }
