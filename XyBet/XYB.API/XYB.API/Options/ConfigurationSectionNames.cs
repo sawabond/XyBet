c0606ebd0163fa407b711e38800e03b1147f393a
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-06-25 23:03:19 +0300

    @XYB-8: Removed usings

--- XyBet/XYB.API/XYB.API/Options/ConfigurationSectionNames.cs
+++ XyBet/XYB.API/XYB.API/Options/ConfigurationSectionNames.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace XYB.API.Options
+﻿namespace XYB.API.Options
 {
     public static class ConfigurationSectionNames
     {
